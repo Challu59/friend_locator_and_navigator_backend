66a79c042abe66955d746e4c8a25e97429482ceb
challu <aayushadhikari72@gmail.com>
2026-05-20 21:29:55 +0545

    feat(user): add CustomTokenObtainPairView to return user information along with authentication tokens

--- navchat/accounts/urls.py
+++ navchat/accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, UserListView
+from .views import RegisterView, UserListView, CustomTokenObtainPairView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='custom_token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/', UserListView.as_view())
 ]
--- navchat/accounts/views.py
+++ navchat/accounts/views.py
@@ -5,11 +5,23 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == 200:
+            serializer = UserSerializer(request.user)
+            response.data['user'] = serializer.data
+        
+        return response
 
 class UserListView(APIView):
     permission_classes = [IsAuthenticated]
