7d35aa788c121e7518e2fe6d278227c85de2f644
challu <aayushadhikari72@gmail.com>
2026-06-02 19:35:02 +0545

    feat(friends): add friendship model

--- navchat/friends/models.py
+++ navchat/friends/models.py
@@ -35,3 +35,26 @@ class FriendRequest(models.Model):
 
     def __str__(self):
         return f'{self.sender} -> {self.receiver}'
+    
+
+class Friendship(models.Model):
+    user1 = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name= 'friendships_one',
+        on_delete=models.CASCADE
+    )
+    user2 = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name= 'friendships_two',
+        on_delete=models.CASCADE
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    class Meta:
+        unique_together = ('user1', 'user2')
+
+    def __str__(self):
+        return f'(self.user1) friend with (self.user2)'
